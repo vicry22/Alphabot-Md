8806a2426a66250ba933204b9a2e0156d07c9608
vicry22 <110922044+vicry22@users.noreply.github.com>
2022-08-09 23:08:22 +0700

    settings.js
    Gk ada

--- settings.js
+++ settings.js
@@ -25,23 +25,23 @@ global.language = indonesia  //change indonesia to english if you don't understa
 
 // Other
 // Other
-global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername= "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
-global.myweb ="https://api.zeeoneofc.xyz" //bebas asal jan hapus
-global.youtube = "https://youtube.com/c/ZeeoneOfc" //bebas asal jan hapus
-global.myweb2 = "https://linktr.ee/zeeoneofc" //bebas
-global.email = "zeeoneofc@gmail.com" //bebas
+global.botname = "VICRY BOTZdོ" //namabot kalian
+global.ownername= "ᴹᴿ᭄ VICRYWIBUcོ ×፝֟͜×" //nama kalian
+global.myweb ="https://api.vicry.xyz" //bebas asal jan hapus
+global.youtube = "https://youtube.com/c/Vicrysus" //bebas asal jan hapus
+global.myweb2 = "https://linktr.ee/vicry" //bebas
+global.email = "akunngab58@gmail.com@gmail.com" //bebas
 global.region = "Indonesia" //bebas
-global.timezone = 'Asia/Jakarta' //  timezone wib
-global.premium = ['62887435047326'] //premium user
-global.owner = ["622150996855","622150996855"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "62887435047326" // nomor wa kalian
-global.ownernomerr = "+62887435047326" //nmr wa kalian
+global.timezone = 'Asia/Malang' //  timezone wib
+global.premium = ['6289513864038'] //premium user
+global.owner = ["6289513864038","6289513864038"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "6289513864038" // nomor wa kalian
+global.ownernomerr = "+6289513864038" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
-global.packname = '© Alphabot-Mdོ' //sticker wm ubah
-global.author = 'Di Buat Oleh ZeeoneOfc' //sticker wm ganti nama kalian
+global.packname = '© VICRY BOTZdོ' //sticker wm ubah
+global.author = 'Di Buat Oleh VicryWibu' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.typemenu = 'document'
 global.multiplier = 69
